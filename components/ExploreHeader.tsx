75445bdd4282c77f6bce6ba5b0e6278a83a1606a
dantr <huynhngdantr@gmail.com>
2023-11-10 18:22:11 +0700

    make the scroll header explore scroll when touched add some rung

--- components/ExploreHeader.tsx
+++ components/ExploreHeader.tsx
@@ -4,29 +4,44 @@ import {
   Text,
   TouchableOpacity,
   SafeAreaView,
+  ScrollView,
+  TextStyle,
 } from "react-native";
-import React from "react";
+import React, { useRef, useState } from "react";
 import {
   FontAwesome5,
   MaterialCommunityIcons,
   Ionicons,
+  MaterialIcons,
 } from "@expo/vector-icons";
 import { Link } from "expo-router";
 import Colors from "@/constants/Colors";
+import * as Haptics from "expo-haptics";
 const categories = [
-  { name: "Tiny Home", icon: "home-outline" },
-  { name: "Private Room", icon: "door-closed" },
-  { name: "Entire Place", icon: "home-city-outline" },
-  { name: "Unique Space", icon: "palette-outline" },
-  { name: "Bed and Breakfast", icon: "bread-slice-outline" },
+  { name: "Tiny Home", icon: "house" },
+  { name: "Private Room", icon: "meeting-room" },
+  { name: "Entire Place", icon: "apartment" },
+  { name: "Unique Space", icon: "emoji-objects" },
+  { name: "Bed and Breakfast", icon: "free-breakfast" },
   { name: "Boutique Hotel", icon: "hotel" },
-  { name: "Shared Room", icon: "account-multiple-outline" },
-  { name: "Farm Stay", icon: "tractor" },
-  { name: "Campsite", icon: "campfire" },
-  { name: "Villa", icon: "home" },
+  { name: "Shared Room", icon: "group" },
+  { name: "Farm Stay", icon: "agriculture" },
+  { name: "Campsite", icon: "fireplace" },
+  { name: "Villa", icon: "house-siding" },
 ];
 
 const ExploreHeader = () => {
+  const scrollRef = useRef<ScrollView>(null);
+  const itemsRef = useRef<Array<TouchableOpacity | null>>([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const selectedCategory = (index: number) => {
+    const selected = itemsRef.current[index];
+    selected?.measure((x) => {
+      scrollRef.current?.scrollTo({ x: x - 16, y: 0, animated: true });
+    });
+    setActiveIndex(index);
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+  };
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
       <View style={styles.container}>
@@ -46,6 +61,44 @@ const ExploreHeader = () => {
             <Ionicons name="options-outline" size={24} />
           </TouchableOpacity>
         </View>
+        <ScrollView
+          ref={scrollRef}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            alignItems: "center",
+            gap: 20,
+            paddingHorizontal: 16,
+          }}
+        >
+          {categories.map((item, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => selectedCategory(index)}
+              ref={(el) => (itemsRef.current[index] = el)}
+              style={
+                activeIndex === index
+                  ? styles.categoriesBtnActive
+                  : styles.categoriesBtn
+              }
+            >
+              <MaterialIcons
+                name={item.icon as any}
+                size={24}
+                color={activeIndex === index ? "#000" : Colors.grey}
+              />
+              <Text
+                style={
+                  activeIndex === index
+                    ? styles.categoryTextActive
+                    : styles.categoryText
+                }
+              >
+                {item.name}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
@@ -85,5 +138,29 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.12,
     shadowRadius: 8,
     shadowOffset: { width: 1, height: 1 },
+  },
+  categoryText: {
+    fontSize: 14,
+    fontFamily: "semibold",
+    color: Colors.grey,
+  },
+  categoryTextActive: {
+    fontSize: 14,
+    fontFamily: "semibold",
+    color: "#000",
+  },
+  categoriesBtn: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 8,
+  },
+  categoriesBtnActive: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 8,
+    borderBottomColor: "#000",
+    borderBottomWidth: 2,
   },
 });
